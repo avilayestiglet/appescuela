1608fa73be823e47bd11131e6aef85ac31d8aa4c
Angel Lugo <angel26078613@gmail.com>
2024-01-11 23:46:18 -0400

    changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import LoginComponent from './funcionalidad/login/login.component';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+      <LoginComponent />
   );
 }
 
